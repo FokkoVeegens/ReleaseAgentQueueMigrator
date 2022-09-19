bbda4d9b7f57e2f0778919d0cac6d7da0cfab526
Fokko <24793348+FokkoVeegens@users.noreply.github.com>
2022-09-19 13:34:41 +0200

    Fit for own use

--- AzureDevopsAgentPoolMigrator/AzureDevopsAgentPoolMigrator/Program.cs
+++ AzureDevopsAgentPoolMigrator/AzureDevopsAgentPoolMigrator/Program.cs
@@ -12,16 +12,20 @@
 
         private const string collection = "<collection>";
 
-        private const string project = "<project>";
+        private static string project;
 
         private const string pat = "<PAT token with sufficient privileges, see Readme>";
 
-        private static readonly string[] rootFoldersToInclude = { "<folder1>", "<folder2>" };
-
-        private const string logFile = "log_migrate_agentpools.txt";
+        private const string logFile = "C:\\temp\\log_migrate_agentpools.txt";
 
         private static async Task Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a Team Project");
+                return;
+            }
+            project = args[0];
             var clientManager = new ClientManager();
             clientManager.ConfigureConnectionAndClients(azureDevopsUri, collection, project, pat);
 
@@ -35,14 +39,13 @@
             Log(string.Empty, includeTimestamp: false);
 
             var migrator = new AgentPoolMigrator(clientManager);
-            await migrator.Migrate(collection, project, rootFoldersToInclude);
+            await migrator.Migrate(collection, project);
 
             Log("\r\n---------------------------------------------------------------", includeTimestamp: false);
 
             Console.WriteLine("Done migrating agent pools");
             var logFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), logFile);
             Console.WriteLine($"Logfile: {logFilePath}");
-            Process.Start("notepad.exe", logFilePath);
         }
 
         public static void Log(string logMessage, bool includeTimestamp = true)
